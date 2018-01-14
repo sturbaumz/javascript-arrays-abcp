b2e964d2be31a0dba2efa0587ccd02172c59bbc1
Zachary Sturbaum <zacharysturbaum@gmail.com>
2018-01-14 08:32:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,2 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-function addElementToBeginningOfArray(chocolateBars, "crunch") {
-  var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-  return ["crunch", ...chocolateBars]}
-function destructivelyAddElementToBeginningOfArray(chocolateBars, "crunch")
-{chocolateBars.unshift("crunch")}
+
